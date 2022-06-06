d0ee55ba929ec1a64e7909c65e96519c8038e036
irina-marzioni-tdf <104698059+irina-marzioni-tdf@users.noreply.github.com>
2022-06-05 21:54:19 -0300

    initial commit

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,17 @@
 let cantidad: number = Number(prompt("ingrese cantidad de butacas que hay"));
-let cantidadButacas: boolean[] = new Array(cantidad);
+let butacas: boolean[] = new Array(cantidad);
 let butacasOcupadas: number = 0;
 let butacasDesocupadas: number = 0;
 
-for (let indice = 0; indice < cantidadButacas.length; indice++) {
-  cantidadButacas[indice] = Math.random() < 0.5;
-  console.log(cantidadButacas[indice]);
-  if (cantidadButacas[indice] === true) {
+for (let indice = 0; indice < butacas.length; indice++) {
+  butacas[indice] = Math.random() < 0.5;
+  console.log(butacas[indice]);
+  if (butacas[indice] === true) {
     butacasOcupadas++;
   } else {
     butacasDesocupadas++;
   }
 }
-console.log(`la cantidad de butacas que hay son ${cantidadButacas.length}`);
+console.log(`La cantidad de butacas que hay son ${butacas.length}`);
 console.log(`Hay ${butacasOcupadas} butacas ocupadas`);
 console.log(`Hay ${butacasDesocupadas} butacas desocupadas`);
