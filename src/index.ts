a051ff0dee94a990ab89acf18591f77c07d4eb6d
NoeliaGaray <103612855+NoeliaGaray@users.noreply.github.com>
2022-06-05 21:30:07 -0300

    terminealfin

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,17 @@
 let cantidad: number = Number(prompt("ingrese cantidad de butacas que hay"));
 let cantidadButacas: boolean[] = new Array(cantidad);
-
-function Aleatorio(ocupada: number = 1, desocupada: number = 0): boolean {
-  return (ocupada === 1) === true && (desocupada === 0) === false;
-}
+let butacasOcupadas: number = 0;
+let butacasDesocupadas: number = 0;
 
 for (let indice = 0; indice < cantidadButacas.length; indice++) {
-  cantidadButacas[indice] = Aleatorio(1, 0);
-  // let desocupadas: number = 0
-  // let ocupadas: number = 1
-  // if (cantidadButacas[indice] === false) {
-  //   desocupadas++;
-  // } else {
-  //   ocupadas++;
-  // }
+  cantidadButacas[indice] = Math.random() < 0.5;
   console.log(cantidadButacas[indice]);
-  // console.log(`En el cine hay ${desocupadas} butacas desocupadas`);
-  // console.log(`En el cine hay ${ocupadas} butacas ocupadas`);
+  if (cantidadButacas[indice] === true) {
+    butacasOcupadas++;
+  } else {
+    butacasDesocupadas++;
+  }
 }
 console.log(`la cantidad de butacas que hay son ${cantidadButacas.length}`);
-
-// function Aleatorio(let butacaOcupada: number = 1, let butacaDesocupada: number = 0): boolean {
-//   return (butacaOcupada===true, butacaDesocupada===false);
-// }
-// function asignarButaca(let butacaOcupada: boolean = 0,)
-//   for (let indice = 0; indice < cantidadButacas.length; indice++){
-// indice += cantidad
-
-// }console.log(`Hay ${cantidadButacas.length} butacas`)
-// }
+console.log(`Hay ${butacasOcupadas} butacas ocupadas`);
+console.log(`Hay ${butacasDesocupadas} butacas desocupadas`);
